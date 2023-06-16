ef6958e631dc0f343b6d6ee4eb707e5c598d15e9
rohit joshi <developerjoshi577@gmail.com>
2023-06-16 21:46:28 +0530

    "1 to n type for loop"

---
+++ average.js
@@ -0,0 +1,5 @@
+function avg(a,b,c,d,e){
+    console.log(`average is ${(a+b+c+d+e)/5}`)
+}
+
+avg(10,5,7,8,9);
---
+++ multi.js
@@ -0,0 +1,7 @@
+function multi(x){
+    for(let i = 1 ; i<=10;i++){
+        console.log(`${x} X ${i} = ${x*i}`)
+    }
+}
+
+multi(5);
